fd96a0fb8fcbd076bd4fb567c4c8666eb18844b0
luke-pbp <lkedziora@pbp.com>
2015-05-21 15:54:02 -0400

    updated port

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World!!!!");
   response.end();
-}).listen(process.env.PORT || 8888);	
+}).listen(process.env.PORT || 1111);	
